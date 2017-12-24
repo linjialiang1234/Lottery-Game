a3c9cc7ce36f972e2df1470c71dee583f5b30cdf
linjialiang1234 <linjialiang86@163.com>
2017-12-25 00:19:50 +1100

    second commit

--- lotteryController.js
+++ lotteryController.js
@@ -1,18 +1,6 @@
 mainApp.controller('lotteryController', function($scope) {
 
-    // $scope.colors = [
-    //     {name: '黑色', color:'black' },
-    //     {name: '白色', color:'white' },
-    //     {name: '红色', color:'red' },
-    //     {name: '蓝色', color:'blue' },
-    //     {name: '黄色', color:'yellow'}
-    // ];
-
-    // $scope.colorChosen = '';
-
     $scope.inputLists = [];
-
-    // $scope.name = "yellow"
 
     $scope.addItem = function() {
         $scope.inputLists.push({
@@ -48,28 +36,27 @@ mainApp.controller('lotteryController', function($scope) {
         var canvas = document.getElementById('canvas');
         var btn = document.getElementById('btn');
         if(!canvas.getContext){
-            alert('抱歉！浏览器不支持。');
+            alert('Sorry, this browser do not support it.');
             return;
         }
 
         totolRotateNumber = 0;
 
-        // 获取绘图上下文
         var ctx = canvas.getContext('2d');
         for (var i = 0; i < num; i++) {
-            // 保存当前状态
+            // save the current state
             ctx.save();
-            // 开始一条新路径
+            // start to draw
             ctx.beginPath();
-            // 位移到圆心，下面需要围绕圆心旋转
+            // the center has been changed
             ctx.translate(150, 150);
-            // 从(0, 0)坐标开始定义一条新的子路径
+            // from(0, 0) define a new route
             ctx.moveTo(0, 0);
-            // 旋转弧度,需将角度转换为弧度,使用 degrees * Math.PI/180 公式进行计算。
+            // uese formular degrees * Math.PI/180 to change degree into arc
             // ctx.rotate((360 / num * i + 360 / num / 2) * Math.PI/180);
             ctx.rotate((360 * (totolRotateNumber/getAllInputPercentage())) * Math.PI/180);
             totolRotateNumber += $scope.inputLists[i].percentage;
-            // 绘制圆弧
+            // draw the arc
             // ctx.arc(0, 0, 150, 0,  2 * Math.PI / num, false);
             ctx.arc(0, 0, 150, 0,  2 * Math.PI *($scope.inputLists[i].percentage/getAllInputPercentage()) , false);
             // if (i % 2 == 0) {
@@ -78,51 +65,36 @@ mainApp.controller('lotteryController', function($scope) {
             //     ctx.fillStyle = '#ffcb3f';
             // }
             ctx.fillStyle = $scope.inputLists[i].color;
-            // 填充扇形
+            // fill the color
             ctx.fill();
-            // 绘制边框
+            // draw outline
             ctx.lineWidth = 0.5;
             ctx.strokeStyle = 'whitesmoke';
             ctx.stroke();
     
-            // 文字
+            // literal
             // ctx.fillStyle = '#fff';
             // ctx.font="16px sans-serif";
             // ctx.fillText(i + 1, 100, 60);
     
-            // 恢复前一个状态
+            //restore previous state
             ctx.restore();
         }
     }
 
-    // $scope.showAlert = function() {
-    //     alert("123");
-    // }
-
-    //$showWinner = false;
-
-    // var abc;
     $scope.rotatePieChart = function() {
-        // console.log(showAlert);
         canvas.style.transform = 'rotate(-' + randomRotateDegree() +'deg)'; 
-        // this.showAlert();
         setTimeout(() => {
-            // $showWinner= true;
-
-            // alert("666");
             var element = document.getElementById("winner");
             element.classList.remove("hidden-winner-text");
         }, 6000);
     }
 
-   
-
     $scope.winner = "";
 
     randomRotateDegree = function() {
-        //获取随机数
+        //get the random number
         var num = parseInt((Math.random()*($scope.inputLists.length)) + 1);
-        // console.log("the number is: " + num);
 
         $scope.winner = $scope.inputLists[num-1].name;
         console.log("the winner is:" + $scope.winner);
@@ -141,8 +113,6 @@ mainApp.controller('lotteryController', function($scope) {
 
         var totalRotateDegree = (3600 + 90 + 360 * (getFinalRoundRotateAngles()/getAllInputPercentage()));
         console.log("123:   " + totalRotateDegree);
-
-        // abc = 360 * (getFinalRoundRotateAngles()/getAllInputPercentage());
 
         return totalRotateDegree;
     }  
